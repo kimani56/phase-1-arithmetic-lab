9718e999627b8f93a13d735d62a4857a45971a83
gift kimani <gift.kimani@students.moringaschool.com>
2023-06-15 20:05:42 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2
+    
+    console.log(multiply);
+
+const random = Math.floor(Math.random() * 10 );
+    console.log(random);
+
+const num3 = 9;
+const num4 = 5;
+let mod = num3 % num4;
+console.log(mod);
+
+const num7 = 17;
+const num8 = 10;
+const num9 =20;
+let max= Math.max(num7, num8, num9);
+
+console.log(max);
+
 
 // Write your code here
